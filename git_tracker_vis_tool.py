72b3614186fbf0c811831b23e37c82712168c9aa
valeriocos <valerio.cosentino@gmail.com>
2014-11-24 18:01:24 +0100

    script to visualize the data stored in the master/evaluation folder

---
+++ git_tracker_vis_tool.py
@@ -0,0 +1,58 @@
+__author__ = 'atlanmod'
+
+import subprocess
+import os
+import signal
+import sys
+import time
+import psutil
+from selenium import webdriver
+from git_tracker_gui import GitTracker
+
+pro = None
+gui = None
+
+
+def start_server():
+    global pro
+    print "Starting server..."
+    cmd = 'python -m SimpleHTTPServer'
+    pro = subprocess.Popen(cmd, shell=True)
+
+
+def open_browser():
+    driver = webdriver.Chrome(executable_path='C:\Program Files (x86)\Google\Chrome\chromedriver.exe')
+    driver.get("http://localhost:8000/index.html")
+    driver.refresh()
+
+    return driver
+
+
+def browser_is_open(driver):
+    open = True
+    try:
+        driver.current_url
+    except:
+        open = False
+
+    return open
+
+
+def shutdown_server():
+    print "Shutting down server..."
+    os.kill(pro.pid, signal.SIGTERM)
+
+
+def main():
+    start_server()
+    driver = open_browser()
+
+    open = True
+    while open:
+        time.sleep(1)
+        open = browser_is_open(driver)
+
+    shutdown_server()
+
+if __name__ == "__main__":
+    main()
